901cdbc4b9155d71556585ccfb988d291a3c652a
mosama88 <mosama88@hotmail>
2024-12-04 17:58:34 +0200

    Done Good2

---
+++ app/Livewire/Dashboard/Auth/Logout.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Auth;
+
+use Livewire\Component;
+
+class Logout extends Component
+{
+    public function render()
+    {
+        return view('dashboard.auth.logout');
+    }
+}
