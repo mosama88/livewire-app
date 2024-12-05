d039400d3440c9c3bcd7cedff6c47809d3bf26c3
mosama88 <mosama88@hotmail>
2024-12-05 15:04:50 +0200

    add index Settings3

--- routes/admin.php
+++ routes/admin.php
@@ -8,14 +8,13 @@ use App\Http\Controllers\Dashboard\SettingController;
 
 Route::prefix('/dashboard/')->name('dashboard.')->group(function(){
     Route::middleware(['admin'])->group(function () {
+        
     // index
     Route::view('index', 'dashboard.index')->name('index');
 
         // Settings
-    Route::view('/settings/edit', 'dashboard.settings.edit')->name('settings.edit');
-
     Route::get('/settings',[SettingController::class,'index'])->name('settings');
-
+    Route::view('/settings/edit', 'dashboard.settings.edit')->name('settings.edit');
 
         
     });
