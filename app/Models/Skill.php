638e75f5be7fab56d8ababa8606ddfd5ae2cae39
mosama88 <mosama88@hotmail>
2024-12-05 16:33:35 +0200

    Stub

---
+++ app/Models/Skill.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+    protected $table = 'skills';
+
+    protected $guarded = [];
+
+
+##-------------------Relations
+/*
+public function department()
+{
+   return $this->belongsTo(Department::class, 'employee_department_code');
+}
+*/
+
+
+
+}
