6b2120b4ca2f0fe708ef3176569104f70cbbe396
mosama88 <mosama88@hotmail>
2024-12-06 13:37:26 +0200

    Done Go Create new skill2

--- app/Livewire/Dashboard/Skills/SkillsCreate.php
+++ app/Livewire/Dashboard/Skills/SkillsCreate.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class SkillsCreate extends Component
 {
+
+
+    public function submit(){
+        dd("Done Submitted");
+    }
+    
     public function render()
     {
         return view('dashboard.skills.skills-create');
--- resources/views/dashboard/skills/skills-create.blade.php
+++ resources/views/dashboard/skills/skills-create.blade.php
@@ -5,25 +5,28 @@
                 <h5 class="modal-title" id="exampleModalLabel1">Add New Skill</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="row g-2">
-                    <div class="col mb-0">
-                        <label for="emailBasic" class="form-label">Name</label>
-                        <input type="text" wire:model="name" class="form-control">
-                    </div>
-                    <div class="col mb-0">
-                        <label class="form-label">Progress</label>
-                        <input type="text" wire:model="progress" class="form-control">
-                    </div>
-                </div>
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                    Close
-                </button>
-                <button type="button" value="submit" class="btn btn-primary">Submit</button>
-            </div>
+            <form wire:submit.prevent = 'submit'>
+                <div class="modal-body">
+                    <div class="row g-2">
+                        <div class="col mb-0">
+                            <label for="emailBasic" class="form-label">Name</label>
+                            <input type="text" wire:model="name" class="form-control">
+                        </div>
+                        <div class="col mb-0">
+                            <label class="form-label">Progress</label>
+                            <input type="text" wire:model="progress" class="form-control">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
