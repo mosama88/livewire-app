0e47e64e65a42baa150bda595f22941446fe3606
mosama88 <mosama88@hotmail>
2024-12-06 15:29:52 +0200

    Done Go Create new skill4

---
+++ app/Livewire/Dashboard/Skills/SkillsUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Skills;
+
+use Livewire\Component;
+
+class SkillsUpdate extends Component
+{
+    public function render()
+    {
+        return view('dashboard.skills.skills-update');
+    }
+}
