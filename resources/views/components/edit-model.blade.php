a78a5d476bc381a5942ac06f66dbe737f17c5599
mosama88 <mosama88@hotmail.com>
2024-12-06 22:58:50 +0200

    Enhance7

---
+++ resources/views/components/edit-model.blade.php
@@ -0,0 +1,29 @@
+{{--
+ wire:ignore.self
+ For Don't Close Modal after Press Submit and investigate Validations
+ --}}
+<div class="modal fade" id="editSkillModal" tabindex="-1" style="display: none;" aria-hidden="true" wire:ignore.self>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel1">{{ $title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form wire:submit.prevent = 'submit'>
+                <div class="modal-body">
+                    <div class="row g-2">
+                        {{ $slot }}
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    @include('dashboard.loadingState', ['buttonName' => 'Update'])
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- resources/views/dashboard/skills/skills-update.blade.php
+++ resources/views/dashboard/skills/skills-update.blade.php
@@ -1,39 +1,13 @@
-{{--
- wire:ignore.self
- For Don't Close Modal after Press Submit and investigate Validations
- --}}
-<div class="modal fade" id="editSkillModal" tabindex="-1" style="display: none;" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Edit Skill</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+<x-edit-model title="Update Skill">
+    <div class="col mb-0">
+        <label for="emailBasic" class="form-label">Name</label>
+        <input type="text" wire:model="name" class="form-control">
+        @include('dashboard.errorValidation', ['property' => 'name'])
 
-            <form wire:submit.prevent = 'submit'>
-                <div class="modal-body">
-                    <div class="row g-2">
-                        <div class="col mb-0">
-                            <label for="emailBasic" class="form-label">Name</label>
-                            <input type="text" wire:model="name" class="form-control">
-                            @include('dashboard.errorValidation', ['property' => 'name'])
-
-                        </div>
-                        <div class="col mb-0">
-                            <label class="form-label">Progress</label>
-                            <input type="text" wire:model="progress" class="form-control">
-                            @include('dashboard.errorValidation', ['property' => 'progress'])
-                        </div>
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    @include('dashboard.loadingState', ['buttonName' => 'Update'])
-                </div>
-            </form>
-        </div>
     </div>
-</div>
+    <div class="col mb-0">
+        <label class="form-label">Progress</label>
+        <input type="text" wire:model="progress" class="form-control">
+        @include('dashboard.errorValidation', ['property' => 'progress'])
+    </div>
+</x-edit-model>
