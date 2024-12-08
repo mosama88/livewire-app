136894af246902fae3a0b04d04d4506fa2e7fa2f
mosama88 <mosama88@hotmail.com>
2024-12-08 20:46:05 +0200

    Done create Counters

---
+++ app/Livewire/Dashboard/Counters/CountersCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Counters;
+
+use Livewire\Component;
+
+class CountersCreate extends Component
+{
+    public function render()
+    {
+        return view('dashboard.counters.counters-create');
+    }
+}
---
+++ app/Livewire/Dashboard/Counters/CountersShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Counters;
+
+use Livewire\Component;
+
+class CountersShow extends Component
+{
+    public function render()
+    {
+        return view('dashboard.counters.counters-show');
+    }
+}
