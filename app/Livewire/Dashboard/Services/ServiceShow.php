9f864516dcb0c54338f4677db095b31024ca66f9
Mohamed Osama <mosama88@hotmail.com>
2024-12-09 09:32:22 +0200

    Services2

--- app/Livewire/Dashboard/Services/ServiceShow.php
+++ app/Livewire/Dashboard/Services/ServiceShow.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire\Dashboard\Services;
 
+use App\Models\Service;
 use Livewire\Component;
 
 class ServiceShow extends Component
 {
+
+    public $name,$description,$icon;
+    
+    protected $listeners = ['ServicesShow'];
+
+    public function ServicesShow($id){
+       $recored=  Service::findOrFail($id);
+       $this->name = $recored->name;
+       $this->description = $recored->description;
+       $this->icon = $recored->icon;
+       $this->dispatch('showModalToggle');
+    }
+    
+
+
     public function render()
     {
         return view('dashboard.services.service-show');
--- resources/views/dashboard/services/service-show.blade.php
+++ resources/views/dashboard/services/service-show.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+<x-show-model title="Show Skill">
+
+    <div class="col-4 mb-0">
+        <label for="emailBasic" class="form-label">Name</label>
+        <div class="form-control">{{ $name }}</div>
+    </div>
+    <div class="col-4 mb-0">
+        <label class="form-label">Description</label>
+        <div class="form-control">{{ $description }}</div>
+    </div>
+    <div class="col-4 mb-0">
+        <label class="form-label">Icon</label>
+        <div class="form-control">{{ $icon }}</div>
+    </div>
+</x-show-model>
