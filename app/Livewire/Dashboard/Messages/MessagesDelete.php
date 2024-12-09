e790037fe746c7fdcfda30e7ef96452c90020282
Mohamed Osama <mosama88@hotmail.com>
2024-12-09 09:35:20 +0200

    Done Good

---
+++ app/Livewire/Dashboard/Messages/MessagesDelete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Messages;
+
+use Livewire\Component;
+
+class MessagesDelete extends Component
+{
+    public function render()
+    {
+        return view('dashboard.messages.messages-delete');
+    }
+}
---
+++ app/Livewire/Dashboard/Messages/MessagesShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Messages;
+
+use Livewire\Component;
+
+class MessagesShow extends Component
+{
+    public function render()
+    {
+        return view('dashboard.messages.messages-show');
+    }
+}
